51132d0193a5caf411bd37ca68888359001be180
hurmat <hurmatkhalid1822@gmail.com>
2023-01-27 23:39:32 +0530

    js buttons

--- cal.js
+++ cal.js
@@ -1,4 +1,14 @@
-const keys = document.querySelectorAll('[data-number]');
-keys.forEach((key) => {
-	key.addEventListener('click', () => console.log(key.innerHTML));
-});
+// const keys = document.querySelectorAll('[data-number]');
+// keys.forEach((key) => {
+// 	key.addEventListener('click', () => console.log(key.innerHTML));
+// });
+const numberButtons = document.querySelectorAll('[data-number');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector(
+	'[data-previous-operand]'
+);
+const currentOperandTextElement = document.querySelector(
+	'[data-current-operand]'
+);
